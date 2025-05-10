03afa53442eac1faba9c87065b44be13247e8931
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2025-05-10 09:28:38 +0200

    feat: Update route

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -7,7 +7,7 @@ const projectController = require('../controllers/projectController');
 router.post('/project-webhook', secretMiddleware, projectController.handleProjectWebhook);
 
 // Route pour le webhook de tâche
-router.post('/webhook', secretMiddleware, projectController.handleTaskWebhook);
+router.post('/task-webhook', secretMiddleware, projectController.handleTaskWebhook);
 
 // Route pour récupérer tous les projets
 router.get('/projects', projectController.getAllProjects);
